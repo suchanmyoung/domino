cf8f42cfe59bf45020c5a0e81033618875d6fe64
suchanmyoung <aud248@naver.com>
2023-02-21 21:21:41 +0900

    Initial commit

---
+++ src/test/java/net/domino/app/DominoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.domino.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DominoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
