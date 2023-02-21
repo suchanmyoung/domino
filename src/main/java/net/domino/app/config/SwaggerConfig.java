eb7def35072f5e29fac40bb7e4857c547b830711
suchanmyoung <aud248@naver.com>
2023-02-22 00:16:57 +0900

    FEAT: Swagger API 명세 추가

---
+++ src/main/java/net/domino/app/config/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package net.domino.app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+            .useDefaultResponseMessages(false)
+            .select()
+            .apis(RequestHandlerSelectors.basePackage("net.domino.app"))
+            .paths(PathSelectors.any())
+            .build()
+            .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+            .title("도미노 API 명세서")
+            .description("도미노 API 명세서입니다.")
+            .version("1.0")
+            .build();
+    }
+}
--- src/main/java/net/domino/app/controller/StockPriceController.java
+++ src/main/java/net/domino/app/controller/StockPriceController.java
@@ -1,5 +1,7 @@
 package net.domino.app.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import net.domino.app.dto.StockPriceResponse;
@@ -8,12 +10,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(value = "주식 종목 조회 API")
 @RequiredArgsConstructor
 @RestController
 public class StockPriceController {
 
     private final StockPriceService stockPriceService;
 
+    @ApiOperation(value = "주식 종목에 대한 5일 간의 시세 데이터를 조회합니다.")
     @GetMapping("/stock/{stockCode}")
     public List<StockPriceResponse> getFiveDaysStockPrice(@PathVariable final String stockCode) {
         return stockPriceService.getFiveDaysStockPrice(stockCode);
