cc20aeaca2f9b8c4f852938b8b43bdbc88a70e1a
suchanmyoung <aud248@naver.com>
2023-02-21 21:26:38 +0900

    FEAT: StockPrice 도메인 클래스 추가

---
+++ src/main/java/net/domino/app/domain/StockPrice.java
@@ -0,0 +1,14 @@
+package net.domino.app.domain;
+
+import java.time.LocalDateTime;
+
+public class StockPrice {
+
+    private LocalDateTime koreanTime;
+    private int open;
+    private int high;
+    private int low;
+    private int close;
+    private int volume;
+    
+}
