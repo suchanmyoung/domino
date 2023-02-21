7fc34a7e9df9ea4ebbc064c52965d756e3a827d6
suchanmyoung <aud248@naver.com>
2023-02-21 21:48:35 +0900

    FEAT: YahooUrlGenerator 유틸 클래스 추가

---
+++ src/main/java/net/domino/app/util/YahooUrlGenerator.java
@@ -0,0 +1,11 @@
+package net.domino.app.util;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class YahooUrlGenerator {
+    public String generate(final String stockCode) {
+        return String.
+            format("https://query1.finance.yahoo.com/v8/finance/chart/%s.KS?interval=1d&range=5d", stockCode);
+    }
+}
