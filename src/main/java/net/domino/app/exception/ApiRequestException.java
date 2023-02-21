888d7a8a76a55656174c116202eaf45718baedb7
suchanmyoung <aud248@naver.com>
2023-02-21 21:59:52 +0900

    REMOVE: 잘못된 URL 호출 시 HttpClientErrorException 발생하여 커스텀 익셉션 제거

--- src/main/java/net/domino/app/exception/ApiRequestException.java
+++
@@ -1,7 +0,0 @@
-package net.domino.app.exception;
-
-public class ApiRequestException extends RuntimeException{
-    public ApiRequestException() {
-        super("야후 API 호출이 실패했습니다.");
-    }
-}
--- src/main/java/net/domino/app/util/YahooApiRequester.java
+++ src/main/java/net/domino/app/util/YahooApiRequester.java
@@ -1,6 +1,5 @@
 package net.domino.app.util;
 
-import net.domino.app.exception.ApiRequestException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -10,12 +9,6 @@ public class YahooApiRequester {
 
     public ResponseEntity<String> request(final String url) {
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
-
-        if (!response.getStatusCode().is2xxSuccessful()) {
-            throw new ApiRequestException();
-        }
-
-        return response;
+        return restTemplate.getForEntity(url, String.class);
     }
 }
